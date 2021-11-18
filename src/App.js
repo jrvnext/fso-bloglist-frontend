3c31fd8e74e1325ed5e670418a3c0c0b4784d29a
Jacobo Rodríguez Viñolo <jacobo.rodriguez@viewnext.com>
2021-11-18 14:26:53 +0100

    Ejercicio 5.2

--- src/App.js
+++ src/App.js
@@ -8,20 +8,29 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
+    loggedUserJSON && setUser(JSON.parse(loggedUserJSON))
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBloglistUser')
+    setUser(null)
+  }
 
   const loginOrBlogs = !user
     ? <Login setUser={setUser} />
     : <>
         <h2>blogs</h2>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
       </>
-  console.log(loginOrBlogs);
+
   return (
     <div>
       {loginOrBlogs}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import loginService from '../services/login'
 
-const Login = ({ user, setUser }) => {
+const Login = ({ setUser }) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -12,42 +12,42 @@ const Login = ({ user, setUser }) => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedBloglistUser',
+        JSON.stringify(user)
+      )
       setUser(user)
-      setUsername('')
-      setPassword('')
     } catch (exception) {
       console.log(exception.message)
     }
   }
 
-  const formOrUser = user
-    ? <p>{user.name} is logged in</p>
-    : <div>
-        <h2>login</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">username</label>
-            <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">password</label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-
-  return formOrUser
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login
